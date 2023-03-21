2271731ae08093ae3b05229276f428aeb4772a6f
Brad Tolson <batolson8605@yahoo.com>
2023-03-21 16:13:58 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react'
+import React from 'react';
 import './App.css';
+import Square from './Square';
+
 
 function App() {
+  const propVariable = 'This is a prop';
+
+  const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
+const [player, setPlayer] = useState(true);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Square
+        squares={squares}
+        setSquares={setSquares}
+        player={player}
+        setPlayer={setPlayer}
+      />
     </div>
   );
 }
 
 export default App;
+
